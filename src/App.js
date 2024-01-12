27e12b7720d0abe0824214ffeb095794dab4022e
Jacob <jlevy03@uoguelph.ca>
2024-01-12 08:48:08 -0500

    fixed start button

--- src/App.js
+++ src/App.js
@@ -1,94 +1,65 @@
 import Card from "./components/card";
 import { Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
-
   const stylesArray = ['card-back-1', 'card-back-1', 'card-back-2', 'card-back-2', 'card-back-3', 'card-back-3', 'card-back-4', 'card-back-4', 'card-back-5', 'card-back-5', 'card-back-6', 'card-back-6'];
-  
-  for (let i = stylesArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [stylesArray[i], stylesArray[j]] = [stylesArray[j], stylesArray[i]];
-  }
+
+    for (let i = stylesArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [stylesArray[i], stylesArray[j]] = [stylesArray[j], stylesArray[i]];
+    }
+
+  const stylesArray1 = [stylesArray[0], stylesArray[1], stylesArray[2]];
+  const stylesArray2 = [stylesArray[3], stylesArray[4], stylesArray[5]];
+  const stylesArray3 = [stylesArray[6], stylesArray[7], stylesArray[8]];
+  const stylesArray4 = [stylesArray[9], stylesArray[10], stylesArray[11]];
+
+  const [cardStates, setCardStates] = useState(Array(12).fill(false));
 
   const handleClick = () => {
+    setCardStates(prevStates => prevStates.map(() => !prevStates[0]));
+
     setTimeout(() => {
-        for (let i = 0; i < 11; i++){
-          
-        }
+      setCardStates(prevStates => prevStates.map(() => !prevStates[0]));
     }, 3000);
   };
 
   return (
     <div className="container">
-
       <button className="startButton" onClick={handleClick}>Start</button>
 
       <Row>
-        <Col >
-          <Card style={stylesArray[0]} id="card0"/>
-        </Col>
-        
-        <Col>
-          <Card style={stylesArray[1]} id="card1"/>
-        </Col>
-
-        <Col>
-          <Card style={stylesArray[2]} id="card2"/>
-        </Col>
+        {stylesArray1.map((style, index) => (
+          <Col key={`card${index}`}>
+            <Card style={stylesArray1[index]} isFlipped={cardStates[index]} />
+          </Col>
+        ))}
       </Row>
-
-      <br></br>
-
       <Row>
-        <Col>
-          <Card style={stylesArray[3]} id="card3"/>
-        </Col>
-        
-        <Col>
-          <Card style={stylesArray[4]} id="card4"/>
-        </Col>
-
-        <Col>
-          <Card style={stylesArray[5]} id="card5"/>
-        </Col>
+        {stylesArray2.map((style, index) => (
+          <Col key={`card${index + 3}`}>
+            <Card style={stylesArray2[index]} isFlipped={cardStates[index + 3]} />
+          </Col>
+        ))}
       </Row>
-
-      <br></br>
-
       <Row>
-        <Col>
-          <Card style={stylesArray[6]} id="card6"/>
-        </Col>
-        
-        <Col>
-          <Card style={stylesArray[7]} id="card7"/>
-        </Col>
-
-        <Col>
-          <Card style={stylesArray[8]} id="card8"/>
-        </Col>
+        {stylesArray3.map((style, index) => (
+          <Col key={`card${index + 6}`}>
+            <Card style={stylesArray3[index]} isFlipped={cardStates[index + 6]} />
+          </Col>
+        ))}
       </Row>
-
-      <br></br>
-
       <Row>
-        <Col>
-          <Card style={stylesArray[9]} id="card9"/>
-        </Col>
-        
-        <Col>
-          <Card style={stylesArray[10]} id="card10"/>
-        </Col>
-
-        <Col>
-          <Card style={stylesArray[11]} id="card11"/>
-        </Col>
+        {stylesArray4.map((style, index) => (
+          <Col key={`card${index + 9}`}>
+            <Card style={stylesArray4[index]} isFlipped={cardStates[index + 9]} />
+          </Col>
+        ))}
       </Row>
-
     </div>
-  )
-
+  );
 }
 
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,25 @@
 import { useState, useEffect } from 'react'
 import ReactCardFlip from 'react-card-flip'
 
-const Card = ({ style }) => {
-    const [isFlipped, setIsFlipped] = useState(false);
-  
-    const flipCard = () => {
-      setIsFlipped(!isFlipped);
-    };
-  
-    return (
-      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-        <div className={`card`} onClick={flipCard}>
-        </div>
-        <div className={`card ${style}`} onClick={flipCard}>
-        </div>
-      </ReactCardFlip>
-    );
+const Card = ({ style, isFlipped }) => {
+  const [internalIsFlipped, setInternalIsFlipped] = useState(false);
+
+  useEffect(() => {
+    setInternalIsFlipped(isFlipped);
+  }, [isFlipped]);
+
+  const flipCard = () => {
+    setInternalIsFlipped(!internalIsFlipped);
   };
 
-export default Card
+  return (
+    <ReactCardFlip isFlipped={internalIsFlipped} flipDirection="vertical">
+      <div className={`card`} onClick={flipCard}>
+      </div>
+      <div className={`card ${style}`} onClick={flipCard}>
+      </div>
+    </ReactCardFlip>
+  );
+};
+
+export default Card;
