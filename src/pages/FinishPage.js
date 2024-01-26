64a43e3a14cf1e23c8737a765859adb62f9148ef
Jacob <jlevy03@uoguelph.ca>
2024-01-26 11:40:22 -0500

    Completed Finish Page

--- src/pages/FinishPage.js
+++ src/pages/FinishPage.js
@@ -15,6 +15,12 @@ function FinishPage() {
       <div className='youWonText'>YOU&nbsp;WON!</div>
       <div className='winnerPhoto'></div>
       <div className='checkEmailText'>CHECK YOUR EMAIL FOR YOUR PRIZE DETAILS</div>
+      <div className='rightRedLine'></div>
+      <div className='shareText'>SHARE</div>
+      <div className='leftRedLine'></div>
+      <div className='facebookLogo'></div>
+      <div className='xLogo'></div>
+      <div className='instagramLogo'></div>
       <div className='finishRedBack'>
         <div className='gradientTop'></div>
         <div className="finishPlayAgainContainer">
