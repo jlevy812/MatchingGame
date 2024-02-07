4529a10e193605d01a68f142813e47bd844da0c6
Jacob <jlevy03@uoguelph.ca>
2024-02-07 09:27:37 -0500

    Finish Page Zoom

--- src/pages/FinishPage.js
+++ src/pages/FinishPage.js
@@ -13,15 +13,15 @@ function FinishPage() {
       <div className='congratsText'>CONGRATS</div>
       <div className='youWonText'>YOU&nbsp;WON!</div>
       <div className='winnerPhoto'></div>
-      <div className='checkEmailText'>CHECK YOUR EMAIL FOR YOUR PRIZE DETAILS</div>
       <div className='leftRedLine'></div>
       <div className='shareText'>SHARE</div>
       <div className='rightRedLine'></div>
       <div className='facebookLogo'></div>
       <div className='xLogo'></div>
       <div className='instagramLogo'></div>
+      <div className='checkEmailText'>CHECK YOUR EMAIL FOR YOUR PRIZE DETAILS</div>
       <div className='finishRedBack'>
-        <div className='gradientTop'></div>
+        <div className='gradientTop finishGradientTop'></div>
         <div className="finishPlayAgainContainer">
           <div className="restartBackground">
             <button className="restartButton" onClick={handleClick}>PLAY AGAIN</button>
