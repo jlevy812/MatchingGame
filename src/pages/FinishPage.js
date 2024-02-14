affff183e9f48b5e001fca56969dbf103d46e6a3
Jacob <jlevy03@uoguelph.ca>
2024-02-14 08:31:25 -0500

    Finish page redo

--- src/pages/FinishPage.js
+++ src/pages/FinishPage.js
@@ -13,12 +13,16 @@ function FinishPage() {
       <div className='congratsText'>CONGRATS</div>
       <div className='youWonText'>YOU&nbsp;WON!</div>
       <div className='winnerPhoto'></div>
-      <div className='leftRedLine'></div>
-      <div className='shareText'>SHARE</div>
-      <div className='rightRedLine'></div>
-      <div className='facebookLogo'></div>
-      <div className='xLogo'></div>
-      <div className='instagramLogo'></div>
+      <div className='shareLine'>
+        <div className='leftRedLine'></div>
+        <div className='shareText'>SHARE</div>
+        <div className='rightRedLine'></div>
+      </div>
+      <div className='logosLine'>
+        <div className='facebookLogo'></div>
+        <div className='xLogo'></div>
+        <div className='instagramLogo'></div>
+      </div>
       <div className='checkEmailText'>CHECK YOUR EMAIL FOR YOUR PRIZE DETAILS</div>
       <div className='finishRedBack'>
         <div className='gradientTop finishGradientTop'></div>
@@ -27,7 +31,7 @@ function FinishPage() {
             <button className="restartButton" onClick={handleClick}>PLAY AGAIN</button>
           </div>
         </div>
-        <div className='gradientBottom'></div>
+        <div className='gradientBottom finishGradientBottom'></div>
       </div>
     </div>
   );
